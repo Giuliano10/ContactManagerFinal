99bf28b16c227c6f7dd6e6f97c374638c528dac1
Giuliano10 <juventinogiuliano@gmail.com>
2021-12-17 12:30:19 -0500

    Change skeleton

---
+++ ContactManagerFinal/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagerFinal
+{
+    class Contact
+    {
+        public Contact( string name, string phoneNumber, string email)
+        {
+
+            this.Name = name;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+
+        }
+
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+
+
+
+
+    }
+}
+
--- ContactManagerFinal/MainWindow.xaml.cs
+++ ContactManagerFinal/MainWindow.xaml.cs
@@ -23,6 +23,64 @@ namespace ContactManagerFinal
         public MainWindow()
         {
             InitializeComponent();
+
+            List<ToDoItem> items = new List<ToDoItem>();
+
+
+            items.Add(new ToDoItem("C# Test", 10));
+            items.Add(new ToDoItem("C# Project", 2));
+            items.Add(new ToDoItem("SQL Practice Question", 80));
+            items.Add(new ToDoItem("SQL Final", 4));
+
+
+            LbToDoList.ItemsSource = items;
+            LbToDoList.SelectedIndex = 0;
+            LbToDoList.Focus();
+
+
+
+
+
+        }
+
+        private void btnShowSelectedItem_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+        }
+
+        private void btnSelectLast_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSelectNext_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSelectCSharp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnShowSelectAll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LbToDoList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnWindow_Click(object sender, RoutedEventArgs e)
+        {
+
+            ExampleNewWIndow eWIndow = new ExampleNewWIndow("Hey Bello");
+            eWIndow.Show();
+
         }
     }
 }
