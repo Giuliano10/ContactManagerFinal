1129607a0cee794cc74790ad45412cb6deab662f
Giuliano10 <juventinogiuliano@gmail.com>
2021-12-17 22:27:43 -0500

    pull req

--- ContactManagerFinal/MainWindow.xaml.cs
+++ ContactManagerFinal/MainWindow.xaml.cs
@@ -27,9 +27,9 @@ namespace ContactManagerFinal
             //List<Contact> contacts = new List<Contact>();
 
 
-            //contacts.Add(new Contact(1, "Luigi", "Migiliara", "514-010-0101", "lumig@gmail.com"));
-            //contacts.Add(new Contact(2, "Giuliano", "Iantomasi", "514-010-0101", "giantomasi@gmail.com"));
-            //contacts.Add(new Contact(3, "Matthew", "Noce", "514-010-0101", "mnoce@gmail.com"));
+            //contacts.Add(new Contact("Luigi", "Migiliara", "514-010-0101", "lumig@gmail.com"));
+            //contacts.Add(new Contact("Giuliano", "Iantomasi", "514-010-0101", "giantomasi@gmail.com"));
+            //contacts.Add(new Contact("Matthew", "Noce", "514-010-0101", "mnoce@gmail.com"));
 
 
             //LbContactManager.ItemsSource = contacts;
@@ -37,15 +37,10 @@ namespace ContactManagerFinal
             //LbContactManager.Focus();
 
 
-
-
-
         }
 
         private void btnAddContact_Click(object sender, RoutedEventArgs e)
         {
-
-
 
         
         }
